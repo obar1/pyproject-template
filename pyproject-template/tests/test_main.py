3ccbb391fb5b9d95f0279ba3f35148aae93aeb62
obar1 <obar1@pm.me>
2023-02-22 14:56:47 +0100

    wip

--- pyproject-template/tests/test_main.py
+++ pyproject-template/tests/test_main.py
@@ -5,7 +5,7 @@ import pytest
 from main import run_main
 
 
-@pytest.disable()
+@pytest.mark.skip(reason="no way of currently testing this")
 def test_run_main(get_args_create_section_processor, get_args_help_processor, http_url):
     """logical seq"""
     run_main(get_args_create_section_processor + [http_url])
