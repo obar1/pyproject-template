6335b91a8ba67b5afc750488f20a27524bbcec57
obar1 <387386+obar1@users.noreply.github.com>
2023-02-22 14:53:07 +0100

    improve code #7
    Co-authored-by: obar1 <obar1@pm.me>

--- pyproject-template/tests/test_persist_fs.py
+++ pyproject-template/tests/test_persist_fs.py
@@ -2,14 +2,18 @@
 import logging
 from typing import List
 
+import pytest
+
 from repository.persist_fs import PersistFS as persist_fs
 
 
+@pytest.disable()
 def test_list_dirs(get_repo_path):
     actual = persist_fs.list_dirs(get_repo_path)
     logging.info(actual)
 
 
+@pytest.disable()
 def test_read_file(get_sample_readme_md_path):
     actual: List[str] = persist_fs.read_file(get_sample_readme_md_path)
     logging.info(actual)
