21e70ee58faa962c5585e32c9663f18bff85bfa0
obar1 <obar1@pm.me>
2023-02-22 14:51:00 +0100

    --wip-- [skip ci]

--- pyproject-template/tests/test_main.py
+++ pyproject-template/tests/test_main.py
@@ -1,9 +1,11 @@
 # pylint: disable=W1203,C0116,R0903,C0114
 
 
+import pytest
 from main import run_main
 
 
+@pytest.disable()
 def test_run_main(get_args_create_section_processor, get_args_help_processor, http_url):
     """logical seq"""
     run_main(get_args_create_section_processor + [http_url])
